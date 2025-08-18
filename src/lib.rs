61a3931b4bb36a515cd71898295017c51fe61f86
Drew Crawford <drew@sealedabstract.com>
2025-08-17 21:45:42 -0500

    Split guard module

--- src/lib.rs
+++ src/lib.rs
@@ -111,91 +111,14 @@
 //! ```
 
 pub mod spinlock;
+pub mod guard;
 
 use crate::spinlock::Spinlock;
+pub use crate::guard::Guard;
 use std::cell::UnsafeCell;
 use std::sync::atomic::AtomicBool;
 use std::thread;
 
-/// A guard that provides access to the data protected by a `Mutex`.
-///
-/// This guard is created by the locking methods on [`Mutex`]. When the guard
-/// is dropped, the lock is automatically released, allowing other threads to
-/// acquire it.
-///
-/// The guard implements `Deref` and `DerefMut`, allowing you to access the
-/// protected data directly.
-///
-/// # Examples
-///
-/// ```
-/// use wasm_safe_mutex::Mutex;
-///
-/// let mutex = Mutex::new(String::from("hello"));
-/// 
-/// // The guard provides access to the protected data
-/// let mut guard = mutex.lock_sync();
-/// guard.push_str(", world!");
-/// assert_eq!(&*guard, "hello, world!");
-/// 
-/// // Lock is released when guard is dropped
-/// drop(guard);
-/// ```
-pub struct Guard<'a, T> {
-    mutex: &'a Mutex<T>,
-    data: &'a mut T,
-}
-
-impl<T> std::ops::Deref for Guard<'_, T> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        self.data
-    }
-}
-
-impl<T> std::ops::DerefMut for Guard<'_, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.data
-    }
-}
-
-impl<T> Drop for Guard<'_, T> {
-    fn drop(&mut self) {
-        // Release the lock
-        self.mutex
-            .data_lock
-            .store(false, std::sync::atomic::Ordering::Release);
-        // Notify any waiting threads
-        self.mutex.did_unlock();
-    }
-}
-
-// Boilerplate trait implementations for Guard
-impl<T: std::fmt::Debug> std::fmt::Debug for Guard<'_, T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Guard")
-            .field("data", &**self)
-            .finish_non_exhaustive()
-    }
-}
-
-impl<T: std::fmt::Display> std::fmt::Display for Guard<'_, T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Display::fmt(&**self, f)
-    }
-}
-
-impl<T> AsRef<T> for Guard<'_, T> {
-    fn as_ref(&self) -> &T {
-        &**self
-    }
-}
-
-impl<T> AsMut<T> for Guard<'_, T> {
-    fn as_mut(&mut self) -> &mut T {
-        &mut **self
-    }
-}
 
 /// Error returned when a lock cannot be immediately acquired.
 ///
