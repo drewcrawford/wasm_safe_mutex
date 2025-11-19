a6fda62768f3cf035412d625a89c1daf6e57a64d
Drew Crawford <drew@sealedabstract.com>
2025-11-18 18:26:45 -0600

    Improve tests

--- src/condvar.rs
+++ src/condvar.rs
@@ -154,10 +154,23 @@ impl Condvar {
         // Release the mutex
         drop(guard);
 
-        // Spin until we can reacquire the lock
-        // In a proper condvar, we'd check if we were notified, but for simplicity
-        // we just yield and try to reacquire
-        std::hint::spin_loop();
+        // Yield to allow other threads/tasks to run
+        // This is critical in WASM where we need to give the event loop a chance
+        #[cfg(target_arch = "wasm32")]
+        {
+            // On WASM, we need to yield back to the event loop
+            // A simple spin_loop won't work because we need cooperative scheduling
+            for _ in 0..100 {
+                std::hint::spin_loop();
+            }
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            // On native, just do a few spin loops to yield CPU
+            for _ in 0..10 {
+                std::hint::spin_loop();
+            }
+        }
 
         // Re-acquire the mutex before returning
         mutex.lock_sync()
@@ -472,3 +485,289 @@ impl Default for Condvar {
 
 unsafe impl Send for Condvar {}
 unsafe impl Sync for Condvar {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Mutex;
+    use std::sync::Arc;
+
+    #[cfg(not(target_arch = "wasm32"))]
+    use std::time::Duration;
+
+    use r#continue::continuation;
+    #[cfg(not(target_arch = "wasm32"))]
+    use std::thread;
+    #[cfg(target_arch = "wasm32")]
+    use wasm_thread as thread;
+
+    // Configure WASM tests to run in browser (required for thread spawning)
+    #[cfg(all(test, target_arch = "wasm32"))]
+    wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+    #[test_executors::async_test]
+    //#[cfg(not(target_arch = "wasm32"))] // HANGS on WASM
+    async fn test_condvar_basic_spin() {
+
+        let pair = Arc::new((Mutex::new(false), Condvar::new()));
+        let pair_clone = Arc::clone(&pair);
+
+        let (c, r) = continuation();
+        thread::Builder::new().name("basic_spin_write".into()).spawn(move || {
+            thread::sleep(std::time::Duration::from_millis(50));
+
+            let (mutex, condvar) = &*pair_clone;
+            let mut ready = mutex.lock_sync();
+            *ready = true;
+            drop(ready);
+            condvar.notify_one();
+            c.send(());
+        }).unwrap();
+        let (c,r2) = continuation();
+        thread::Builder::new().name("basic_spin_write".into()).spawn(move || {
+            let (mutex, condvar) = &*pair;
+            let mut ready = mutex.lock_sync();
+            while !*ready {
+                ready = condvar.wait_spin(ready);
+            }
+            assert!(*ready);
+            c.send(());
+        }).unwrap();
+        r.await;
+        r2.await;
+    }
+
+    #[test_executors::async_test]
+    async fn test_condvar_wait_block() {
+        let pair = Arc::new((Mutex::new(false), Condvar::new()));
+        let pair_clone = Arc::clone(&pair);
+
+        let (c, r) = continuation();
+        thread::spawn(move || {
+            #[cfg(not(target_arch = "wasm32"))]
+            thread::sleep(Duration::from_millis(10));
+
+            let (mutex, condvar) = &*pair_clone;
+            let mut ready = mutex.lock_sync();
+            *ready = true;
+            drop(ready);
+            condvar.notify_one();
+            c.send(());
+        });
+
+        // Move the waiting into a worker thread
+        let (c2, r2) = continuation();
+        thread::spawn(move || {
+            let (mutex, condvar) = &*pair;
+            let mut ready = mutex.lock_sync();
+            while !*ready {
+                ready = condvar.wait_block(ready);
+            }
+            assert!(*ready);
+            c2.send(());
+        });
+
+        r.await;
+        r2.await;
+    }
+
+    #[test_executors::async_test]
+    async fn test_condvar_wait_sync() {
+        let pair = Arc::new((Mutex::new(false), Condvar::new()));
+        let pair_clone = Arc::clone(&pair);
+
+        let (c, r) = continuation();
+        thread::spawn(move || {
+            #[cfg(not(target_arch = "wasm32"))]
+            thread::sleep(Duration::from_millis(10));
+
+            let (mutex, condvar) = &*pair_clone;
+            let mut ready = mutex.lock_sync();
+            *ready = true;
+            drop(ready);
+            condvar.notify_one();
+            c.send(());
+        });
+
+        // Move the waiting into a worker thread
+        let (c2, r2) = continuation();
+        thread::spawn(move || {
+            let (mutex, condvar) = &*pair;
+            let mut ready = mutex.lock_sync();
+            while !*ready {
+                ready = condvar.wait_sync(ready);
+            }
+            assert!(*ready);
+            c2.send(());
+        });
+
+        r.await;
+        r2.await;
+    }
+
+    #[test_executors::async_test]
+    async fn test_condvar_wait_async() {
+        let pair = Arc::new((Mutex::new(false), Condvar::new()));
+        let pair_clone = Arc::clone(&pair);
+
+        let (c, r) = continuation();
+        thread::spawn(move || {
+            #[cfg(not(target_arch = "wasm32"))]
+            thread::sleep(Duration::from_millis(10));
+
+            test_executors::spin_on(async {
+                let (mutex, condvar) = &*pair_clone;
+                let mut ready = mutex.lock_async().await;
+                *ready = true;
+                drop(ready);
+                condvar.notify_one();
+            });
+            c.send(());
+        });
+
+        // Move the async waiting into a worker thread
+        let (c2, r2) = continuation();
+        thread::spawn(move || {
+            test_executors::spin_on(async {
+                let (mutex, condvar) = &*pair;
+                let mut ready = mutex.lock_async().await;
+                while !*ready {
+                    ready = condvar.wait_async(ready).await;
+                }
+                assert!(*ready);
+            });
+            c2.send(());
+        });
+
+        r.await;
+        r2.await;
+    }
+
+    #[test_executors::async_test]
+    async fn test_condvar_notify_all() {
+        let pair = Arc::new((Mutex::new(0), Condvar::new()));
+        let mut receivers = Vec::new();
+
+        // Spawn 3 threads that wait for count to reach 10
+        for _ in 0..3 {
+            let pair = Arc::clone(&pair);
+            let (c, r) = continuation();
+            thread::spawn(move || {
+                let (mutex, condvar) = &*pair;
+                let mut count = mutex.lock_sync();
+                while *count < 10 {
+                    count = condvar.wait_sync(count);
+                }
+                c.send(*count);
+            });
+            receivers.push(r);
+        }
+
+        // Give threads time to start waiting
+        #[cfg(not(target_arch = "wasm32"))]
+        thread::sleep(Duration::from_millis(50));
+
+        // Update the count and notify all
+        let (mutex, condvar) = &*pair;
+        let mut count = mutex.lock_sync();
+        *count = 10;
+        drop(count);
+        condvar.notify_all();
+
+        // All threads should wake up and see count = 10
+        for receiver in receivers {
+            let result = receiver.await;
+            assert_eq!(result, 10);
+        }
+    }
+
+    #[test_executors::async_test]
+    async fn test_condvar_producer_consumer() {
+        use std::collections::VecDeque;
+
+        let shared = Arc::new((Mutex::new(VecDeque::new()), Condvar::new()));
+        let producer = Arc::clone(&shared);
+
+        let (c, r) = continuation();
+        thread::spawn(move || {
+            let (mutex, condvar) = &*producer;
+            for i in 0..5 {
+                #[cfg(not(target_arch = "wasm32"))]
+                thread::sleep(Duration::from_millis(5));
+
+                let mut queue = mutex.lock_sync();
+                queue.push_back(i);
+                drop(queue);
+                condvar.notify_one();
+            }
+            c.send(());
+        });
+
+        // Move the consumer (waiting) into a worker thread
+        let consumer = Arc::clone(&shared);
+        let (c2, r2) = continuation();
+        thread::spawn(move || {
+            let (mutex, condvar) = &*consumer;
+            let mut collected = Vec::new();
+            for _ in 0..5 {
+                let mut queue = mutex.lock_sync();
+                while queue.is_empty() {
+                    queue = condvar.wait_sync(queue);
+                }
+                if let Some(value) = queue.pop_front() {
+                    collected.push(value);
+                }
+            }
+            assert_eq!(collected, vec![0, 1, 2, 3, 4]);
+            c2.send(());
+        });
+
+        r.await;
+        r2.await;
+    }
+
+    #[test_executors::async_test]
+    async fn test_condvar_notify_one_only_wakes_one() {
+        let pair = Arc::new((Mutex::new(0), Condvar::new()));
+        let mut receivers = Vec::new();
+
+        // Spawn 3 threads that increment on each wake
+        for _ in 0..3 {
+            let pair = Arc::clone(&pair);
+            let (c, r) = continuation();
+            thread::spawn(move || {
+                let (mutex, condvar) = &*pair;
+                let mut count = mutex.lock_sync();
+                while *count < 3 {
+                    count = condvar.wait_spin(count);
+                }
+                c.send(());
+            });
+            receivers.push(r);
+        }
+
+        // Give threads time to start waiting
+        #[cfg(not(target_arch = "wasm32"))]
+        thread::sleep(Duration::from_millis(50));
+
+        // Increment and notify one at a time
+        for i in 1..=3 {
+            let (mutex, condvar) = &*pair;
+            let mut count = mutex.lock_sync();
+            *count = i;
+            drop(count);
+            condvar.notify_one();
+
+            #[cfg(not(target_arch = "wasm32"))]
+            thread::sleep(Duration::from_millis(10));
+        }
+
+        // All threads should eventually complete
+        for receiver in receivers {
+            receiver.await;
+        }
+
+        let (mutex, _) = &*pair;
+        assert_eq!(*mutex.lock_sync(), 3);
+    }
+}
