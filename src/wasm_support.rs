4a242d9876beca1ae4ef2eb2482938db1e02005e
Drew Crawford <drew@sealedabstract.com>
2026-02-01 09:40:03 -0600

    Use wasm_support

--- src/wasm_support.rs
+++ src/wasm_support.rs
@@ -27,3 +27,8 @@ extern "C" {
 pub(crate) fn atomics_wait_supported() -> bool {
     supportsAtomicsWait()
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+pub(crate) fn atomics_wait_supported() -> bool {
+    true
+}
